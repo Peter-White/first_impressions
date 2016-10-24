174806f923ddae3c1146affa4e57978744c09efe
Peter-White <pwhite007@icloud.com>
2016-10-24 11:45:42 -0400

    Changed ul to ol,  added heroku remote

--- public/javascripts/postData.js
+++ public/javascripts/postData.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
     format: "json"
   };
   function displayReviews(data) {
-    var reviewHTML = '<ul>';
+    var reviewHTML = '<ol>';
     $.each(data, function(i, review) {
       reviewHTML += '<li><h2>' + review.title + '</h2></li>';
       reviewHTML += '<li><p>' + review.content + '</p></li>';
       reviewHTML += '<li><p>' + review.rating + '</p></li>';
     });
-    reviewHTML += '</ul>';
+    reviewHTML += '</ol>';
     $('.reviews').html(reviewHTML);
   };
   $.getJSON(reviewAPI, reviewOptions, displayReviews);
