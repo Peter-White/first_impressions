eb7958f30254e8a7c7b29d7172fd90ab13d138ad
Peter-White <pwhite007@icloud.com>
2016-10-23 22:37:17 -0400

    First post successful

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'First Impressions' });
 });
 
+router.get('/new', function(req, res) {
+  res.render('post', { title: 'Test' });
+});
+
 module.exports = router;
