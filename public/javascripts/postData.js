008f31550c8d06a41524ffe247afd2020588a3e5
Peter-White <pwhite007@icloud.com>
2016-10-24 11:37:31 -0400

    Ajax Get Successful

--- public/javascripts/postData.js
+++ public/javascripts/postData.js
@@ -1,3 +1,17 @@
-$(function(){
-
+$(document).ready(function() {
+  var reviewAPI = "/reviews";
+  var reviewOptions = {
+    format: "json"
+  };
+  function displayReviews(data) {
+    var reviewHTML = '<ul>';
+    $.each(data, function(i, review) {
+      reviewHTML += '<li><h2>' + review.title + '</h2></li>';
+      reviewHTML += '<li><p>' + review.content + '</p></li>';
+      reviewHTML += '<li><p>' + review.rating + '</p></li>';
+    });
+    reviewHTML += '</ul>';
+    $('.reviews').html(reviewHTML);
+  };
+  $.getJSON(reviewAPI, reviewOptions, displayReviews);
 });
