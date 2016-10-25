1e3f2b2702b65191b9281eb768c760bb38badf95
Peter-White <pwhite007@icloud.com>
2016-10-25 13:57:58 -0400

    Keyup fix applied to counter

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   var $textarea = $('.postReview form textarea');
   var textLength;
 
-$textarea.on('keydown', function() {
+$textarea.on('keyup', function() {
     textLength = $(this).val().length;
-    $(".textlength").html(textLength);
+    $('.charCount').html(textLength);
 });
 
 });
