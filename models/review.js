5174e5f529957fbbdb623bc4b120ec291071ac5d
Peter-White <pwhite007@icloud.com>
2016-10-26 15:21:53 -0400

    Started thumbs radio function

--- models/review.js
+++ models/review.js
@@ -5,7 +5,7 @@ var reviewSchema = new mongoose.Schema({
   title: String,
   content: String,
   created_at: { type: Date },
-  rating: Number
+  rating: String
 });
 
 reviewSchema.pre('save', function(next){
