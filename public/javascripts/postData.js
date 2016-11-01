c56d2fa5e94750b33ea5234d13d71a68b66144a7
Peter-White <pwhite007@icloud.com>
2016-11-01 00:55:04 -0400

    More mobile styling

--- public/javascripts/postData.js
+++ public/javascripts/postData.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     var reviewHTML = '<div class="reviews">';
     $.each(data, function(i, review) {
       reviewHTML += '<ul type="none" id="' + review._id + '" class=' + qualityCheck(review.rating) + '>';
-      reviewHTML += '<li><img src="' + review.rating + '" height="70" id="thumb"/></li>';
+      reviewHTML += '<li><img src="' + review.rating + '" id="thumb"/></li>';
       reviewHTML += '<li><h2 id="title">' + review.title + '</h2></li>';
       reviewHTML += '<li><p id="review">' + review.content + '</p></li>';
       reviewHTML += '</ul>';
